0c954de12ec9e949578f14eed75d5a3f633142ef
Sergii-7 <s.beshlyaga1979@gmail.com>
2024-09-24 22:33:43 +0300

    22:33 24.09.2024

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # запуск fast-api
-uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1 &
+uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Запуск Telegram бота
 python3 run_telegram_bot.py &
