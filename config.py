a53213e73d23dc287c571c7cea79667c185aa220
Sergii-7 <s.beshlyaga1979@gmail.com>
2024-09-24 21:59:34 +0300

    21:59 24.09.2024

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ load_dotenv(find_dotenv())
 
 """ Визначаємо шлях до файлів з логами відносно кореневої директорії проекту """
 # Отримуємо абсолютний шлях до кореневої директорії проекту
-# project_root = os.path.dirname(os.path.abspath(__file__))
-file_log_tel_bot = 'logs/tel_bot.log'
-file_log_fast_api = 'logs/fast_api.log'
+project_root = os.path.dirname(os.path.abspath(__file__))
+file_log_tel_bot = os.path.join(project_root, 'logs', 'tel_bot.log')
+file_log_fast_api = os.path.join(project_root, 'logs', 'fast_api.log')
 
 """ DataBase 'birthday_bot' on Linux Ubuntu in Digital Ocean """
 URI_DB = os.environ.get('UBUNTU_URI_DB')
